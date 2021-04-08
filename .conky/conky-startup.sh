b1f435f0ce6b794f5a24641aa69b16b00a42eca4
Fahim Montasir Misbah <fahimscirex@tutanota.com>
2021-04-08 21:56:31 +0600

    initial commit

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -11,6 +11,13 @@ if [ "$DESKTOP_SESSION" = "gnome" ]; then
    conky -c "$HOME/.conky/spoclo/spotify-top/spoclo_rightedge.conky" &
    exit 0
 fi
+if [ "$DESKTOP_SESSION" = "i3" ]; then 
+   sleep 5s
+   killall conky
+   cd "$HOME/.conky/i3"
+   conky -c "$HOME/.conky/i3/Process" &
+   exit 0
+fi
 if [ "$DESKTOP_SESSION" = "gnome-xorg" ]; then 
    sleep 5s
    killall conky
@@ -18,7 +25,18 @@ if [ "$DESKTOP_SESSION" = "gnome-xorg" ]; then
    conky -c "$HOME/.config/conky/Process" &
    cd "$HOME/.conky/Sirius"
    conky -c "$HOME/.conky/Sirius/Sirius" &
-   cd "$HOME/.conky"
-   conky -c "$HOME/.conky/bar" &
+   cd "$HOME/.conky/spoclo/spotify-top"
+   conky -c "$HOME/.conky/spoclo/spotify-top/spoclo_rightedge.conky" &
+   exit 0
+fi
+if [ "$DESKTOP_SESSION" = "bspwm" ]; then 
+   sleep 5s
+   killall conky
+   cd "$HOME/.conky/wm"
+   conky -c "$HOME/.conky/wm/Process" &
+   cd "$HOME/.conky/wm"
+   conky -c "$HOME/.conky/wm/Sirius" &
+   cd "$HOME/.conky/wm"
+   conky -c "$HOME/.conky/wm/Spoclo" &
    exit 0
 fi
---
+++ .conky/conky.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+   cd "$HOME/.conky/wm"
+   conky -c Process
+   conky -c Spoclo
+   conky -c Sirius
+   exit 0
